91f1c8cfd8ce083d9c342b468db646f18df75e2d
Teddyy1101 <nguyenhuythu111@gmail.com>
2026-06-28 20:17:54 +0700

    update midleware

--- middleware.ts
+++ middleware.ts
@@ -2,22 +2,30 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
+  const { pathname } = request.nextUrl;
+
+  if (
+    pathname.startsWith('/_next') ||
+    pathname.startsWith('/api') ||
+    pathname.startsWith('/favicon') ||
+    pathname.startsWith('/images') ||
+    pathname.includes('.')
+  ) {
+    return NextResponse.next();
+  }
+
   const token = request.cookies.get('accessToken')?.value;
-  const currentPath = request.nextUrl.pathname;
-  const isLoginPage = currentPath.startsWith('/login');
+  const isLoginPage = pathname.startsWith('/login');
 
   if (!token && !isLoginPage) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
-
   if (token && isLoginPage) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 
   return NextResponse.next();
 }
-
-// Cấu hình những route nào Middleware này sẽ chạy qua
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|images).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|images|.*\\..*).*)'],
 };
